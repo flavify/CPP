e69da3a7998d6463ea64424a887369fb4401714d
Flavia Voicu <flavia.voicuu@gmail.com>
2024-10-08 16:21:39 +0200

    Add 'documentation'

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -10,4 +10,5 @@ template <typename T> void swap(T &a, T &b) {
 /*Function template to return the minimum of 2 values*/
 template <typename T> T min(const T &a, const T &b) { return (a < b) ? a : b; }
 
+/*Function template to return the maximum of 2 values*/
 template <typename T> T max(const T &a, const T &b) { return (a > b) ? a : b; }
