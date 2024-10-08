295b46440f3afac70117e0a9ec7a7693da373a21
Flavia Voicu <flavia.voicuu@gmail.com>
2024-10-08 16:44:26 +0200

    Array implementation finished

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -2,7 +2,7 @@
 
 template <typename T> class Array {
 private:
-  T *elements; // point to dynamically alocated array
+  T *elements;
   int len;
 
 public:
@@ -14,17 +14,32 @@ public:
 
   Array<T> &operator=(const Array<T> &other) {
     if (this != &other) {
-      delete[] elements; // Free the old memory
+      delete[] elements;
 
       length = other.length;
       elements = new T[length];
       for (unsigned int i = 0; i < length; ++i) {
-        elements[i] = other.elements[i]; // Deep copy each element
+        elements[i] = other.elements[i];
       }
     }
     return *this;
   }
 
   ~Array() { delete[] elements };
-	
+
+  T &operator[](int idx) {
+    if (idx >= len) {
+      throw std::out_of_range("Array index out of bounds");
+    }
+    return elements[idx];
+  }
+
+  const T &operator[](unsigned int index) const {
+    if (index >= length) {
+      throw std::out_of_range("Array index out of bounds");
+    }
+    return elements[index];
+  }
+
+  int size() const { return len; }
 };
