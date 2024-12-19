30628cc829f549dd1fe3df1602a575486ac92f89
dendeaisd <flavia.voicuu@gmail.com>
2024-12-19 06:58:11 +0200

    add main

---
+++ ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+    return 1;
+  }
+
+  try {
+    BitcoinExchange btc("bitcoin_database.csv"); // Replace with your database filename
+    btc.processInputFile(argv[1]);
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
