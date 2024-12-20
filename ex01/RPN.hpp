a64cee225262381dc7cbab313ca5858cd3092c9e
dendeaisd <flavia.voicuu@gmail.com>
2024-12-20 23:31:06 +0200

    RPN in OCF

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -9,7 +9,11 @@
 class RPN {
 	public:
   	int evaluate();
+		RPN() = default;
   	RPN(const std::string &expression);
+		RPN(const RPN &other) = default;
+		RPN &operator=(const RPN &other) = default;
+		~RPN() = default;
 
 	private:
   	std::string expression;
